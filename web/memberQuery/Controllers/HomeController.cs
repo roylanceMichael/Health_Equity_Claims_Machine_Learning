80d1df5af25ea35952da7231624c3c58f1396b62
roylanceMichael <roylance.michael@gmail.com>
2014-04-07 12:42:46 -0600

    final upload

--- web/memberQuery/Controllers/HomeController.cs
+++ web/memberQuery/Controllers/HomeController.cs
@@ -19,31 +19,38 @@
 
 		public ActionResult Index(string birthYear, string state, string previousCpts)
 		{
-			if (!string.IsNullOrWhiteSpace(birthYear))
+			int year;
+
+			if (string.IsNullOrWhiteSpace(birthYear) ||
+				!int.TryParse(birthYear, out year) ||
+				string.IsNullOrWhiteSpace(state) ||
+				string.IsNullOrWhiteSpace(previousCpts))
 			{
-				var cacheRepository = new CacheRepository(this.connectionString);
-				var results = cacheRepository.GetQueryResults(birthYear, state, previousCpts);
-
-				if (results != null)
-				{
-					return this.View(results);
-				}
-
-				var queryResults = new QueryResultsBuilder(this.connectionString, birthYear, state, previousCpts).Build();
-				queryResults.BirthYear = birthYear;
-				queryResults.Location = state;
-				queryResults.CptCodes = previousCpts;
-				cacheRepository.CacheQueryResults(birthYear, state, previousCpts, queryResults);
-				return this.View(queryResults);
+				return
+					this.View(
+						new QueryResults
+							{
+								BirthYear = birthYear,
+								CptCodes = previousCpts,
+								Location = state,
+								Results = new TransitionPredictions(new List<TransitionEmissions>())
+							});
 			}
 
-			return this.View(new QueryResults
-				                 {
-													 BirthYear = birthYear,
-													 CptCodes = previousCpts,
-													 Location = state,
-					                 Results = new TransitionPredictions(new List<TransitionEmissions>())
-				                 });
+			var cacheRepository = new CacheRepository(this.connectionString);
+			var results = cacheRepository.GetQueryResults(birthYear, state, previousCpts);
+
+			if (results != null)
+			{
+				return this.View(results);
+			}
+
+			var queryResults = new QueryResultsBuilder(this.connectionString, birthYear, state, previousCpts).Build();
+			queryResults.BirthYear = birthYear;
+			queryResults.Location = state;
+			queryResults.CptCodes = previousCpts;
+			cacheRepository.CacheQueryResults(birthYear, state, previousCpts, queryResults);
+			return this.View(queryResults);
 		}
 
 		public ActionResult PredictResults()
